0d8151103d167c23c79959a904cbd03985d57823
Qi He <qhe@heyook.com>
2014-11-19 10:44:57 -0800

    require open struct

--- spec/riot_api/requester_spec.rb
+++ spec/riot_api/requester_spec.rb
@@ -1,29 +1,30 @@
 require 'spec_helper'
+require 'ostruct'
 
 describe RiotAPI::Requester do
-  
+
   let(:request_url)   { "https://prod.api.pvp.net/api/lol/na/v1.4/summoner/by-name/tiqoo" }
   let(:mock_strategy) { OpenStruct.new summoner: request_url }
   let(:requester)     { mock_strategy.extend(RiotAPI::Requester) }
   let(:conn)          { requester.conn }
   let(:file)          { File.read File.join(File.expand_path(File.dirname(__FILE__)), "../fixtures/summoner.json") }
   let(:response)      { OpenStruct.new body: file }
-  
+
   before do
     RiotAPI::API.stub(:key) { "abc" }
   end
-  
+
   describe "#find" do
-    
+
     it "calls only when find is present" do
       expect(conn).to receive(:get).with(request_url, {:api_key=>"abc"}).and_return(response)
       requester.find_summoner
     end
-    
+
     it 'returns error if strategy do not respond to the action' do
       expect{ requester.find_unkown }.to raise_error
     end
-    
+
   end
-  
+
 end
